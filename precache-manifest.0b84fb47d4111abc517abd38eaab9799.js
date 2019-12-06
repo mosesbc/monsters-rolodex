d7da4cc9e3f49e28b8fc1507c6fd02f60845e48d
mosesbc <mosescalma@gmail.com>
2019-12-06 23:39:18 +0800

    Updates

---
+++ precache-manifest.0b84fb47d4111abc517abd38eaab9799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf057e3a200f2b4ede719ee6d3e8600a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c191ea378993dbc06dcf",
+    "url": "/monsters-rolodex/static/css/main.569b2f01.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c191ea378993dbc06dcf",
+    "url": "/monsters-rolodex/static/js/main.1e9aecd4.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
